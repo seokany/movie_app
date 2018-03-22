0af3527efdb9d92e9beb4a3d47addc874703c749
seokany <seokhwan.h.yi@gmail.com>
2018-03-23 02:50:50 +0900

    ,

--- src/App.js
+++ src/App.js
@@ -16,30 +16,32 @@ class App extends Component {
     const movies = this.state.movies.map(movie => {
       console.log(movie)
       return <Movie 
-        title={movie.title} poster={movie.medium_cover_image} 
+        title={movie.title}
+        poster={movie.medium_cover_image} 
         key={movie.id} genres={movie.genres} 
         synopsis={movie.synopsis} />
     })
-    return movies
+    return movies;
   }
 
   _getMovies = async () => {
     const  movies = await this._callApi()
     this.setState({
       movies
-    })
-  }
+    });
+  };
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
     .then(potato => potato.json())
     .then(json => json.data.movies) 
     .catch(err => console.log(err))
   }
   render() {
+    const {movies} = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
         
       </div>
     );
